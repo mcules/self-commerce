766dd00bd1eb34347df5988131fc197b6e3a6cd9
DennisEisold <de@eisold-edv.de>
2014-02-18 00:27:22 +0100

    Fixed Bug with only one language in contentmanager

--- admin/c_m_content.php
+++ admin/c_m_content.php
@@ -18,23 +18,25 @@ $query_first_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id = '".$
 $sql_first_lang = xtc_db_query($query_first_lang);
 $first_language = xtc_db_fetch_array($sql_first_lang);
 
-// 2. sprache setzen 
-if($count_languages==2){// wenn nur 2 vorhanden die andere wählen
-  $query_sec_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id <> '".$_SESSION['languages_id']."'";
+if($count_languages > 1) {
+	// 2. sprache setzen 
+	if($count_languages==2){// wenn nur 2 vorhanden die andere wählen
+		$query_sec_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id <> '".$_SESSION['languages_id']."'";
+	}
+	if($count_languages>2){// wenn > 2 auswahl über formular
+		if($_POST['second_languages_id'] =='' && $_SESSION['second_language_id'] == ''){// irgendeine sprache setzen
+			$query_sec_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id <> '".$_SESSION['languages_id']."'";
+		}elseif($_SESSION['second_language_id'] != '' && $_POST['second_languages_id'] !=''){// formular sprache setzen
+			$query_sec_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id = '".$_POST['second_languages_id']."'";
+		}else{// $_SESSION['second_language_id'] nutzen
+			$query_sec_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id = '".$_SESSION['second_language_id']."'";  
+		}
+	}
+	$sql_sec_lang = xtc_db_query($query_sec_lang);
+	$second_language = xtc_db_fetch_array($sql_sec_lang);
+	$_SESSION['second_language_id'] = $second_language['languages_id'];
+	// end 2. sprache setzen
 }
-if($count_languages>2){// wenn > 2 auswahl über formular
-  if($_POST['second_languages_id'] =='' && $_SESSION['second_language_id'] == ''){// irgendeine sprache setzen
-    $query_sec_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id <> '".$_SESSION['languages_id']."'";
-  }elseif($_SESSION['second_language_id'] != '' && $_POST['second_languages_id'] !=''){// formular sprache setzen
-    $query_sec_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id = '".$_POST['second_languages_id']."'";
-  }else{// $_SESSION['second_language_id'] nutzen
-    $query_sec_lang = "SELECT * FROM ".TABLE_LANGUAGES." WHERE languages_id = '".$_SESSION['second_language_id']."'";  
-  }
-}
-$sql_sec_lang = xtc_db_query($query_sec_lang);
-$second_language = xtc_db_fetch_array($sql_sec_lang);
-$_SESSION['second_language_id'] = $second_language['languages_id'];
-// end 2. sprache setzen
 
 $sec_languages_select = '';
 if($count_languages>2){// formular nur benötigt wenn > 2 sprachen
