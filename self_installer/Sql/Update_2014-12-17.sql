c954cce698fc0a79b5509fc6c4bf6431985e5ef3
DennisEisold <de@eisold-edv.de>
2014-12-17 03:31:59 +0200

    Fixed Issue #17

--- self_installer/Sql/Update_2014-12-17.sql
+++ self_installer/Sql/Update_2014-12-17.sql
@@ -2,3 +2,5 @@ ALTER TABLE products ADD products_han VARCHAR( 30 ) NOT NULL; ALTER TABLE produc
 CREATE TABLE IF NOT EXISTS suppliers_info ( suppliers_id int(11) NOT NULL, suppliers_url varchar(255) NOT NULL, suppliers_customer_number varchar(30) NOT NULL, PRIMARY KEY (suppliers_id) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS suppliers ( suppliers_id int(11) NOT NULL AUTO_INCREMENT, suppliers_name varchar(32) NOT NULL, suppliers_image varchar(64) DEFAULT NULL, date_added datetime DEFAULT NULL, last_modified datetime DEFAULT NULL, PRIMARY KEY (suppliers_id), KEY IDX_SUPPLIERS_NAME (suppliers_name) ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=5;
 ALTER TABLE admin_access ADD suppliers INT( 1 ) NOT NULL DEFAULT '1';
+INSERT INTO products_details (products_details_id, products_details_name, products_details_title) VALUES
+(1, 'Details', '{"2":"","1":""}');
